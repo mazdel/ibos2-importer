ad04e970245876f3f11e08c45e078d184814d7b5
MazDel <65859631+mazdel@users.noreply.github.com>
2020-09-25 21:29:43 +0700

    Update ibos2autoinput.js

--- ibos2autoinput.js
+++ ibos2autoinput.js
@@ -1,30 +1,33 @@
+//jquery is needed
 const ibos2autoinput = (targetRows,data)=>{
     $(targetRows).each((key,value)=>{
-    const cell             = $(value).children();
-    const gameNameCell     = $(cell[3]);
-    const coinMeterCell         = $(cell[4]);
-    const ticketCounterCell     = $(cell[5]);
-    const coinMeterInput        = $(coinMeterCell.find("input"));
-    const ticketCounterInput    = $(ticketCounterCell.find("input"));
-    
-    /*deklarasi data*/
-    data.forEach((item,key)=>{
-        const dataGameName = item[0];
-        const dataCoinMeter = item[1];
-        const dataTicketCounter = item[2];
+        //deklarasi letak data
+        const cell             = $(value).children();
+        const gameNameCell     = $(cell[3]);
+        const coinMeterCell         = $(cell[4]);
+        const ticketCounterCell     = $(cell[5]);
+        const coinMeterInput        = $(coinMeterCell.find("input"));
+        const ticketCounterInput    = $(ticketCounterCell.find("input"));
 
-        if(gameNameCell.text()==dataGameName){
-            coinMeterInput.val(dataCoinMeter);
-            if(ticketCounterInput.val()>=0){
-                ticketCounterInput.val(dataTicketCounter);
+        /*deklarasi data*/
+        data.forEach((item,key)=>{
+            const dataGameName = item[0];
+            const dataCoinMeter = item[1];
+            const dataTicketCounter = item[2];
+
+            if(gameNameCell.text()==dataGameName){
+                coinMeterInput.val(dataCoinMeter);
+                if(ticketCounterInput.val()>=0){
+                    ticketCounterInput.val(dataTicketCounter);
+                }
+                console.log(
+                    `${dataGameName} is done with`,
+                    `C => ${coinMeterInput.val()}`,
+                    `T => ${ticketCounterInput.val()}
+                    `);
             }
-            console.log(
-                `${dataGameName} is done with`,
-                `C => ${coinMeterInput.val()}`,
-                `T => ${ticketCounterInput.val()}
-                `);
-        }
+        })
+        //console.log(key,ticketCounterInput.val());
     })
-    //console.log(key,ticketCounterInput.val());
-})
 }
+ibos2autoinput('tbody>tr[role=row]',data);
